522ddb2bcf277e8f1c93637bb900b93c8bb6e480
Wédson Gomes <40505932+wedgomes@users.noreply.github.com>
2022-07-03 02:54:52 -0300

    Adicionando tag de concluido

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,16 @@ const App = () => {
       },
     ]);
 
+    const handleTaskClick = (taskId) => {
+      const newTasks = tasks.map((task) =>{
+        if (task.id === taskId) return { ... task, completed: !task.completed};
+
+          return task;
+      });
+
+      setTasks(newTasks);
+    };
+
     const handleTaskAddition = (taskTitle) => {
       const newTasks = [... tasks, {
         title: taskTitle,
@@ -31,8 +41,8 @@ const App = () => {
 
     return (
           <div className="container">
-              <AddTask handleTaskAddition={handleTaskAddition}/>
-              <Tasks tasks={tasks}/>
+              <AddTask handleTaskAddition={handleTaskAddition} />
+              <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
           </div>
     );
 }
